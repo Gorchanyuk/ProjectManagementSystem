7f1e587fd261a8f134082cf6439c7dff88a67da4
Gorchanyuk <gorchanyk@gmail.com>
2023-05-04 16:04:44 +0400

    Initial commit

---
+++ src/main/java/didgital/design/project_management_system/ProjectManagementSystem.java
@@ -0,0 +1,7 @@
+package didgital.design.project_management_system;
+
+public class ProjectManagementSystem {
+    public static void main(String[] args) {
+        System.out.println("qwwe");
+    }
+}
