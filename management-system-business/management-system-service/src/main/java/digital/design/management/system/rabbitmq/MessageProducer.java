1b51f1ae3a6ad43a00a4ce36c47c1559c0da37e0
Gorchanyuk <gorchanyk@gmail.com>
2023-06-14 11:52:28 +0400

    Подправил документацию

--- management-system-business/management-system-service/src/main/java/digital/design/management/system/rabbitmq/MessageProducer.java
+++ management-system-business/management-system-service/src/main/java/digital/design/management/system/rabbitmq/MessageProducer.java
@@ -8,9 +8,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
-@Slf4j
 public class MessageProducer {
 
     private final RabbitTemplate rabbitTemplate;
